d70f5991b9599bd788c3267c19f49aefea14cad9
Yash Mahajan <mahajanyash2185@gmail.com>
2026-01-21 01:00:53 +0530

    Decorators in python

---
+++ 08_decorators/02_solution.py
@@ -0,0 +1,20 @@
+# Create a decorator to print the function name and the values of its arguments every time the function is called.
+
+def debug(func):
+    def wrapper(*args, **kwargs):
+        args_value = ', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f"{k}={v}" for k, v in kwargs.items())
+        print(f"calling: {func.__name__} with args {args_value} and kwargs {kwargs_value}")
+        return func(*args, **kwargs)    
+    return wrapper
+
+@debug
+def hello():
+    print("Hello")
+
+@debug
+def greet (name, greeting="Hello"):
+    print(f"{greeting}, {name}")
+    
+hello()
+greet("Chai", greeting="Namaste")
