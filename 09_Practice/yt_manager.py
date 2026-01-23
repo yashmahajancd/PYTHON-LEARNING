639825f41eaf5d3bef14ffa78e592b4678f7e955
Yash Mahajan <mahajanyash2185@gmail.com>
2026-01-23 23:44:20 +0530

    Youtube Manager App Project - Practice

--- 09_Practice/yt_manager.py
+++ 09_Practice/yt_manager.py
@@ -1,30 +1,60 @@
 import json
 
+def load_data():
+    try:
+        with open('yt.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data_helper(videos):
+    with open('yt.txt', 'w') as file:
+        json.dump(videos, file)
 
 def list_all_videos(videos):
-    pass
+    print("=" * 50)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. Name: {video['name']}, Time: {video['time']}")
+    print("=" * 50)
 
 def add_video(videos):
-    pass
+    name = input("Enter video name : ")
+    time = input("Enter video time : ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update : "))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name : ")
+        time = input("Enter the new video time : ")
+        videos[index-1] = {'name': name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected!")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted : "))
+    if 1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected!")
 
 
 def main():
-    videos = []
+    videos = load_data()
     while True:
-        print("Youtube Manager App | Choose an Option\n")
-        print("-" * 20)
+        print("Youtube Manager App | Choose an Option")
+        print("-" * 40)
         print("1. List all Youtube Videos")
         print("2. Add a Youtube Video")
         print("3. Update a Youtube Video Details")
         print("4. Delete a Youtube Video")
         print("5. Exit")
-        print("-" * 20)
+        print("-" * 40)
         choose = input("Enter Your Choice : ")
         
         match choose:
